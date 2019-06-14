350f2efc1a6c9ec97db83a08166a77c00a3933eb
Saadjava4 <Saadjava4@gmail.com>
2019-06-14 22:10:32 +0500

    Initial commit

---
+++ lib/chat-models.dart
@@ -0,0 +1,61 @@
+
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+  List<ChatModel> dummyData = [
+    new ChatModel(
+      name: "Saad",
+      message: "How are you man! ",
+      time: "11:13",
+      avatarUrl: "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500"
+
+    ),
+    new ChatModel(
+        name: "Mark",
+        message: "What about the project ",
+        time: "12:15",
+        avatarUrl: "https://i.redd.it/6onq25y0sh311.jpg"
+
+    ),
+    new ChatModel(
+        name: "Ali",
+        message: "Call me Later I am busy ",
+        time: "4:15",
+        avatarUrl: "http://face-agency.co.uk/images/uploads/models/large/1470043935-21.jpg"
+
+    ),
+    new ChatModel(
+        name: "Punk",
+        message: "Check the mail ",
+        time: "2:15",
+        avatarUrl: "https://pbs.twimg.com/profile_images/1055263632861343745/vIqzOHXj.jpg"
+
+    ),
+    new ChatModel(
+        name: "Wahaj",
+        message: "Assigment bhej yar",
+        time: "10:05",
+        avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt46HykT-gRe8Mqnnat1mCNKQJ-y_Ip_bVKixn-ilK15mnRy6p"
+
+    ),
+    new ChatModel(
+        name: "Mustafa",
+        message: "Sath chalty hy kal ",
+        time: "12:00",
+        avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe055ZYMEKL2DVG4KkntX8qQV8kCesJXE1L2JiRinc6Xbr7B7z"
+
+    ),
+    new ChatModel(
+        name: "Zain",
+        message: "Sabeel lagaon ga ajana kal",
+        time: "1:15",
+        avatarUrl: "https://nadiazheng.com/wp-content/uploads/2017/01/Montreal-business-portrait-professional-accoutant-linkedin-profile.jpg"
+
+    )
+  ];
